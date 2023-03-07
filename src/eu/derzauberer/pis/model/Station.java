034271f7554b18fb7164ecd919f11321a7095e38
Der-Zauberer <and.som282@gmail.com>
2023-03-07 21:31:07 +0100

    feat: station service model

--- src/eu/derzauberer/pis/model/Station.java
+++ src/eu/derzauberer/pis/model/Station.java
@@ -8,13 +8,14 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
-@JsonPropertyOrder({"id", "name", "platforms", "adress"})
+@JsonPropertyOrder({"id", "name", "platforms", "adress", "services"})
 public class Station implements Entity<String> {
 	
 	private final String id;
 	private final String name;
 	private final ArrayList<Integer> platforms = new ArrayList<>();
 	private final Adress adress = new Adress();
+	private final StationServices services = new StationServices();
 
 	public Station(String name) {
 		this(Entity.nameToId(name), name);
@@ -42,6 +43,10 @@ public class Station implements Entity<String> {
 	
 	public Adress getAdress() {
 		return adress;
+	}
+	
+	public StationServices getServices() {
+		return services;
 	}
 
 }
---
+++ src/eu/derzauberer/pis/model/StationServices.java
@@ -0,0 +1,120 @@
+package eu.derzauberer.pis.model;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+
+@JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
+@JsonPropertyOrder({"parking", "bicycleParking", "localPublicTransport", "publicFacilities", "taxiRank", "hasCarRental", "lockerSystem", "travelNecessities", "barrierFree", "wifi", "travelCenter", "railwayMission"})
+public class StationServices {
+	
+	private boolean parking = false;
+	private boolean bicycleParking = false;
+	private boolean localPublicTransport = false;
+	private boolean publicFacilities = false;
+	private boolean taxiRank = false;
+	private boolean hasCarRental = false;
+	private boolean lockerSystem = false;
+	private boolean travelNecessities = false;
+	private boolean barrierFree = false;
+	private boolean wifi = false;
+	private boolean travelCenter = false;
+	private boolean railwayMission = false;
+	
+	public boolean hasParking() {
+		return parking;
+	}
+	
+	public void setParking(boolean parking) {
+		this.parking = parking;
+	}
+	
+	public boolean hasBicycleParking() {
+		return bicycleParking;
+	}
+	
+	public void setBicycleParking(boolean bicycleParking) {
+		this.bicycleParking = bicycleParking;
+	}
+	
+	public boolean hasLocalPublicTransport() {
+		return localPublicTransport;
+	}
+	
+	public void setLocalPublicTransport(boolean localPublicTransport) {
+		this.localPublicTransport = localPublicTransport;
+	}
+	
+	public boolean hasPublicFacilities() {
+		return publicFacilities;
+	}
+	
+	public void setPublicFacilities(boolean publicFacilities) {
+		this.publicFacilities = publicFacilities;
+	}
+	
+	public boolean hasTaxiRank() {
+		return taxiRank;
+	}
+	
+	public void setTaxiRank(boolean taxiRank) {
+		this.taxiRank = taxiRank;
+	}
+	
+	public boolean hasHasCarRental() {
+		return hasCarRental;
+	}
+	
+	public void setHasCarRental(boolean hasCarRental) {
+		this.hasCarRental = hasCarRental;
+	}
+	
+	public boolean hasLockerSystem() {
+		return lockerSystem;
+	}
+	
+	public void setLockerSystem(boolean lockerSystem) {
+		this.lockerSystem = lockerSystem;
+	}
+	
+	public boolean hasTravelNecessities() {
+		return travelNecessities;
+	}
+	
+	public void setTravelNecessities(boolean travelNecessities) {
+		this.travelNecessities = travelNecessities;
+	}
+	
+	public boolean hasBarrierFree() {
+		return barrierFree;
+	}
+	
+	public void setBarrierFree(boolean barrierFree) {
+		this.barrierFree = barrierFree;
+	}
+	
+	public boolean hasWifi() {
+		return wifi;
+	}
+	
+	public void setWifi(boolean wifi) {
+		this.wifi = wifi;
+	}
+	
+	public boolean hasTravelCenter() {
+		return travelCenter;
+	}
+	
+	public void setTravelCenter(boolean travelCenter) {
+		this.travelCenter = travelCenter;
+	}
+	
+	public boolean hasRailwayMission() {
+		return railwayMission;
+	}
+	
+	public void setRailwayMission(boolean railwayMission) {
+		this.railwayMission = railwayMission;
+	}
+
+}

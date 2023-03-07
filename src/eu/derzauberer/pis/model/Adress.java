f0feae9090bc2347035dfd5494f5022819c320b9
Der-Zauberer <and.som282@gmail.com>
2023-03-07 21:06:42 +0100

    fix: adress model

--- src/eu/derzauberer/pis/model/Adress.java
+++ src/eu/derzauberer/pis/model/Adress.java
@@ -5,12 +5,11 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
-@JsonPropertyOrder({"name", "street" , "number", "postalCode", "city", "state", "country", "telephoneNumber", "mail"})
+@JsonPropertyOrder({"name", "street", "postalCode", "city", "state", "country", "telephoneNumber", "mail"})
 public class Adress {
 	
 	private String name;
 	private String street;
-	private String number;
 	private Integer postalCode;
 	private String city;
 	private String state;
@@ -21,54 +20,63 @@ public class Adress {
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getStreet() {
 		return street;
 	}
+	
 	public void setStreet(String street) {
 		this.street = street;
 	}
-	public String getNumber() {
-		return number;
-	}
-	public void setNumber(String number) {
-		this.number = number;
-	}
+	
 	public Integer getPostalCode() {
 		return postalCode;
 	}
+	
 	public void setPostalCode(Integer postalCode) {
 		this.postalCode = postalCode;
 	}
+	
 	public String getCity() {
 		return city;
 	}
+	
 	public void setCity(String city) {
 		this.city = city;
 	}
+	
 	public String getState() {
 		return state;
 	}
+	
 	public void setState(String state) {
 		this.state = state;
 	}
+	
 	public String getCountry() {
 		return country;
 	}
+	
 	public void setCountry(String country) {
 		this.country = country;
 	}
+	
 	public Integer getTelephoneNumber() {
 		return telephoneNumber;
 	}
+	
 	public void setTelephoneNumber(Integer telephoneNumber) {
 		this.telephoneNumber = telephoneNumber;
 	}
+	
 	public String getMail() {
 		return mail;
 	}
+	
 	public void setMail(String mail) {
 		this.mail = mail;
 	}

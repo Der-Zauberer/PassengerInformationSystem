1e48cde1415468922d04f7522c252ade60cf985b
Der-Zauberer <and.som282@gmail.com>
2023-03-11 23:32:51 +0100

    fix: codestyle

--- src/eu/derzauberer/pis/downloader/DataDownloader.java
+++ src/eu/derzauberer/pis/downloader/DataDownloader.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Map;
@@ -29,46 +30,52 @@ public abstract class DataDownloader {
 	
 	public Optional<JsonNode> download(String url, Map<String, String> parameters, Map<String, String> header) {
 		LOGGER.info("Downloading " + name + " from " + url);
-		final String fullUrl = url + getParameterString(parameters);
 		try {
-			final HttpURLConnection connection = (HttpURLConnection) new URL(fullUrl).openConnection();
-			connection.setRequestMethod("GET");
-			header.forEach((key, value) -> {
-				connection.setRequestProperty(key, value);
-			});
-			connection.setConnectTimeout(7000);
-			connection.setReadTimeout(7000);
-			BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String responseLine;
-			StringBuilder response = new StringBuilder();
-			while ((responseLine = input.readLine()) != null) {
-			    response.append(responseLine);
-			}
-			input.close();
+			final HttpURLConnection connection = connect(url, parameters, header);
+			final String response = readResponse(connection);
 			connection.disconnect();
 			LOGGER.info("Processing " + name + " from " + url);
 			return Optional.of(new ObjectMapper().readValue(response.toString(), ObjectNode.class));
 		} catch (IOException exception) {
 			exception.printStackTrace();
-			LOGGER.info("Failed to download " + name + " from " + fullUrl);
+			LOGGER.info("Failed to download " + name + " from " + url);
 			return Optional.empty();
 		}
 	}
 	
-	private String getParameterString(Map<String, String> parameters) {
-		try {
-			StringBuilder result = new StringBuilder();
-			for (Map.Entry<String, String> entry : parameters.entrySet()) {
-				result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-				result.append("=");
-				result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-				result.append("&");
-			}
-			String resultString = result.toString();
-			return resultString.length() > 0 ? resultString.substring(0, resultString.length() - 1) : resultString;
-		} catch (UnsupportedEncodingException exception) {
-			return null;
+	private String getParameterString(Map<String, String> parameters) throws UnsupportedEncodingException {
+		StringBuilder result = new StringBuilder();
+		for (Map.Entry<String, String> entry : parameters.entrySet()) {
+			result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+			result.append("=");
+			result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+			result.append("&");
 		}
+		String resultString = result.toString();
+		return resultString.length() > 0 ? resultString.substring(0, resultString.length() - 1) : resultString;
+	}
+	
+	private HttpURLConnection connect(String url, Map<String, String> parameters, Map<String, String> header) throws MalformedURLException, IOException {
+		final String fullUrl = url + getParameterString(parameters);
+		final HttpURLConnection connection = (HttpURLConnection) new URL(fullUrl).openConnection();
+		connection.setRequestMethod("GET");
+		header.forEach((key, value) -> {
+			connection.setRequestProperty(key, value);
+		});
+		connection.setConnectTimeout(7000);
+		connection.setReadTimeout(7000);
+		return connection;
+	}
+	
+	private String readResponse(HttpURLConnection connection) throws IOException {
+		final BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String responseLine;
+		StringBuilder response = new StringBuilder();
+		while ((responseLine = input.readLine()) != null) {
+		    response.append(responseLine);
+		}
+		input.close();
+		return response.toString();
 	}
 	
 	public String getName() {

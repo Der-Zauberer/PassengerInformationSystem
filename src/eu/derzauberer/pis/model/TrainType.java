ae2b38e46a05d102bbe138d1c2d5c6c909bf7d21
Der-Zauberer <and.som282@gmail.com>
2023-03-25 11:57:01 +0100

    feat: train classification

--- src/eu/derzauberer/pis/model/TrainType.java
+++ src/eu/derzauberer/pis/model/TrainType.java
@@ -3,14 +3,22 @@ package eu.derzauberer.pis.model;
 import java.beans.ConstructorProperties;
 
 public class TrainType implements Entity {
+	
+	public enum TrainClassifican {
+		FREIGHT,
+		PASSENGER_REGIONAL,
+		PASSENGER_DISTANCE
+	}
 
 	private final String id;
 	private final String name;
+	private final TrainClassifican classification;
 	
-	@ConstructorProperties({"id", "name"})
-	public TrainType(String id, String name) {
+	@ConstructorProperties({"id", "name", "classification"})
+	public TrainType(String id, String name, TrainClassifican classification) {
 		this.id = id;
 		this.name = name;
+		this.classification = classification;
 	}
 	
 	@Override
@@ -21,6 +29,10 @@ public class TrainType implements Entity {
 	@Override
 	public String getName() {
 		return name;
+	}
+	
+	public TrainClassifican getClassification() {
+		return classification;
 	}
 
 }

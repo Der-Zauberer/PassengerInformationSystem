d9b8f86f5dbd978e866740d31bcfd86de72df6c1
Der-Zauberer <and.somfeat282@gmail.com>
2023-04-19 21:18:25 +0200

    feat: search

--- src/eu/derzauberer/pis/service/StationService.java
+++ src/eu/derzauberer/pis/service/StationService.java
@@ -1,16 +1,27 @@
 package eu.derzauberer.pis.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import eu.derzauberer.pis.model.Station;
 import eu.derzauberer.pis.util.EntityService;
 import eu.derzauberer.pis.util.MemoryRepository;
+import eu.derzauberer.pis.util.SearchTree;
 
 @Service
 public class StationService extends EntityService<Station> {
 	
+	private final SearchTree<Station> search = new SearchTree<>(getRepository()); 
+	
 	public StationService() {
 		super("stations", new MemoryRepository<>("stations", Station.class));
+		getRepository().setAddAction(search::add);
+		getRepository().getList().forEach(search::add);
+	}
+	
+	public List<Station> searchByName(String name) {
+		return search.searchByName(name);
 	}
 
 }
---
+++ src/eu/derzauberer/pis/util/SearchTree.java
@@ -0,0 +1,64 @@
+package eu.derzauberer.pis.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SearchTree<T extends Entity<T>> {
+	
+	private final Map<String, List<String>> entries = new HashMap<>();
+	private final Repository<T> repository;
+	
+	public SearchTree(Repository<T> repository) {
+		this.repository = repository;
+	}
+	
+	public void add(T entity) {
+		for (String searchString : getSearchStrings(entity.getName())) {
+			for (int i = 0; i < searchString.length(); i++) {
+				final String subString = searchString.substring(0, i + 1);
+				List<String> results;
+				if ((results = entries.get(subString)) == null) {
+					results = new ArrayList<>();
+					entries.put(subString, results);
+				}
+				results.add(entity.getId());
+			}
+		}
+	}
+	
+	public void remove(T entity) {
+		for (String searchString : getSearchStrings(entity.getName())) {
+			for (int i = 0; i < searchString.length(); i++) {
+				final String subString = searchString.substring(0, i + 1);
+				List<String> results;
+				if ((results = entries.get(subString)) != null) {
+					results.remove(entity.getId());
+					if (results.isEmpty()) {
+						entries.remove(subString);
+					}
+				}
+			}
+		}
+	}
+	
+	public List<T> searchByName(String name) {
+		final List<T> results = new ArrayList<>();
+		final List<String> resultIds;
+		if ((resultIds = entries.get(name)) == null) return results;
+		for (String id : resultIds) {
+			repository.getById(id).ifPresent(results::add);
+		}
+		return results;
+	}
+	
+	private String[] getSearchStrings(String string) {
+		return string.toLowerCase()
+				.replaceAll("-{1}", " ")
+				.replaceAll("[^A-Za-z0-9\\r\\n\\t\\f\\v ]", "")
+				.replaceAll("\\s{2,}", " ")
+				.split("\\s");
+	}
+
+}

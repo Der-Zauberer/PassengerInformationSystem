039e5334a83c7a98e3338f50672911fbc5fb4bec
Der-Zauberer <and.som282@gmail.com>
2023-05-29 17:06:14 +0200

    feat: route id

--- src/eu/derzauberer/pis/service/RouteService.java
+++ src/eu/derzauberer/pis/service/RouteService.java
@@ -1,15 +1,21 @@
 package eu.derzauberer.pis.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import eu.derzauberer.pis.model.Route;
 import eu.derzauberer.pis.repositories.Repository;
 
+@Service
 public class RouteService extends EntityService<Route> {
 
 	@Autowired
 	public RouteService(Repository<Route> repository) {
 		super(repository);
+	}
+	
+	public String createRouteId() {
+		return String.format("%08x", Long.valueOf(System.nanoTime()).toString().hashCode());
 	}
 
 }

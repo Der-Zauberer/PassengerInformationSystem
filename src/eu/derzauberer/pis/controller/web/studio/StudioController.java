8f5ed18545294aa614545d161275b0f090de3ce6
Der-Zauberer <and.som282@gmail.com>
2023-07-11 22:35:36 +0200

    feat: studio pages

---
+++ src/eu/derzauberer/pis/controller/web/studio/StudioController.java
@@ -0,0 +1,21 @@
+package eu.derzauberer.pis.controller.web.studio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/studio")
+public class StudioController {
+	
+	@GetMapping
+	public String getDashboardPage() {
+		return "/studio/dashboard.html";
+	}
+	
+	@GetMapping("/stations")
+	public String getStationsPage() {
+		return "/studio/stations.html";
+	}
+
+}

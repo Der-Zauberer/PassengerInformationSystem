0d5f4958a64d199500cb5869bd28dce32a962007
Der-Zauberer <and.som282@gmail.com>
2023-08-05 23:23:19 +0200

    feat: import export status notification

--- resources/static/resources/pis/script.js
+++ resources/static/resources/pis/script.js
@@ -35,7 +35,11 @@ class PIS {
 	/* import          */
 	/*******************/
 	
+	#importExportBlocker = false;
+	
 	import(url) {
+		if (this.#importExportBlocker) return;
+		this.#importExportBlocker = true;
 		const input = document.createElement('input');
 		input.type = 'file';
 		input.accept = "application/json"
@@ -43,6 +47,8 @@ class PIS {
 			if(!window.FileReader) return;
 			const fileReader = new FileReader();
 			fileReader.onload = function(event) {
+				document.getElementById('import-export-notification-text').innerText = 'Importing ...';
+				swd.toggleNotification('import-export-notification');
         		fetch(url, {method: 'POST', body: event.target.result, signal: AbortSignal.timeout(120000) }).then(event => {
 					location.reload();
 				});
@@ -53,8 +59,33 @@ class PIS {
 	}
 	
 	export(url) {
-		window.location.href = url; 
-		//fetch(url, { signal: AbortSignal.timeout(120000) }).then((event) => console.log(event));
+		if (this.#importExportBlocker) return;
+		this.#importExportBlocker = true;
+		document.getElementById('import-export-notification-text').innerText = 'Exporting ...';
+		swd.toggleNotification('import-export-notification');
+		var fileName = 'unknown.json'
+		fetch(url, { signal: AbortSignal.timeout(120000) })
+		
+		.then(result => {
+	        const disposition = result.headers.get('Content-Disposition');
+	        fileName = disposition.split(/;(.+)/)[1].split(/=(.+)/)[1];
+	        if (fileName.toLowerCase().startsWith("utf-8''"))
+	            fileName = decodeURIComponent(fileName.replace(/utf-8''/i, ''));
+	        else
+	            fileName = fileName.replace(/['"]/g, '');
+	        return result.blob();
+	    })
+	    .then(blob => {
+	        const url = window.URL.createObjectURL(blob);
+	        const link = document.createElement('a');
+	        link.href = url;
+	        link.download = fileName;
+	        document.body.appendChild(link);
+	        link.click();
+	        link.remove();
+	        swd.toggleNotification('import-export-notification');
+	        this.#importExportBlocker = false;
+	    });
 	}
 	
 	/*******************/

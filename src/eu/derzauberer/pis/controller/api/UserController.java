f59160a190b773407ed5eae150d53690371eff92
Der-Zauberer <and.som282@gmail.com>
2023-11-28 07:37:03 +0100

    fix: put support removal

--- src/eu/derzauberer/pis/controller/api/UserController.java
+++ src/eu/derzauberer/pis/controller/api/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -63,13 +62,6 @@ public class UserController {
 	@PostMapping
 	public UserData setUser(@RequestBody UserForm userForm) {
 		final User user = userFormConverter.convertToModel(userForm);
-		return userDataConverter.convert(userService.save(user));
-	}
-	
-	@PutMapping
-	public UserData updateUser(@RequestBody UserForm userForm) {
-		final User user = userService.getById(userForm.getId()).orElseThrow(() -> getNotFoundException(userForm.getId()));
-		userFormConverter.convertToModel(user, userForm);
 		return userDataConverter.convert(userService.save(user));
 	}
 	

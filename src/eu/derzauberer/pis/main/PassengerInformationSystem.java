723a12a5061359a6672055b3025d9b0df22e09ee
Der-Zauberer <and.som282@gmail.com>
2024-01-26 20:43:51 +0100

    feat: properties file

--- src/eu/derzauberer/pis/main/PassengerInformationSystem.java
+++ src/eu/derzauberer/pis/main/PassengerInformationSystem.java
@@ -3,7 +3,6 @@ package eu.derzauberer.pis.main;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,19 +15,12 @@ import eu.derzauberer.pis.configuration.SpringConfiguration;
 @ComponentScan(basePackages = {"eu.derzauberer.pis"})
 public class PassengerInformationSystem {
 	
-	private static final SpringApplication springApplication = new SpringApplication(PassengerInformationSystem.class);
-	
 	public static void main(String[] args) throws IOException {
 		final List<String> arguments = Arrays.stream(args).map(arg -> arg.toLowerCase()).toList();
 		if (arguments.contains("--no-caching")) SpringConfiguration.caching = false;
 		if (arguments.contains("--no-indexing")) SpringConfiguration.indexing = false;
 		
-		final Properties properties = new Properties();
-		properties.put("server.error.include-message", "always");
-		properties.put("spring.servlet.multipart.max-file-size", "50MB");
-		properties.put("spring.servlet.multipart.max-request-size", "50MB");
-		springApplication.setDefaultProperties(properties);
-		
+		final SpringApplication springApplication = new SpringApplication(PassengerInformationSystem.class);
 		springApplication.run();
 	}
 
